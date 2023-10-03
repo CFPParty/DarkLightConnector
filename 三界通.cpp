7147094867664da6d6d5fd78773ed2787600642f
CFPParty <133478563+CFPParty@users.noreply.github.com>
2023-10-03 14:36:13 +0800

    Add files via upload

---
+++ 三界通.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	system("curl -JLO https://github.com/CFPParty/cfpparty.github.io/archive/refs/heads/20230721.zip");
+	system("move cfpparty* ../中国四民党官网源代码.zip");
+	system("curl -JLO https://github.com/hello-world-1989/whyyoutouzhele/archive/refs/heads/main.zip");
+	system("move whyyou* ../李老师推文每月归档.zip");
+	system("curl -JLO https://github.com/hello-world-1989/cn-news/archive/refs/heads/main.zip");
+	system("move cn-news* ../反贼推文大集合每小时归档.zip");
+	system("curl -JLO https://github.com/hello-world-1989/program-think-image/archive/refs/heads/main.zip");
+	system("move program* ../编程随想翻墙指南和反厂卫指南长期归档.zip");
+	system("pause");
+}
